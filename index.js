5961e17bc098c815db85351f4c2b900349a22768
edensongjbs <edensongjbs@gmail.com>
2020-09-18 13:36:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function findMinAndRemove(array){
-
+    let minIndex=0, min=array[0]
+    for (let i=0; i<array.length; i++) {
+        if (array[i]<min) {
+            minIndex = i
+            min=array[i]
+        }
+    }
+    array.splice(minIndex, 1)
+    return min
 }
 
 function selectionSort(array){
-
+    let sortedArray = [], numElements=array.length
+    for (let i=0; i<numElements; i++) {
+        sortedArray.push(findMinAndRemove(array))
+    }
+    return sortedArray
 }
